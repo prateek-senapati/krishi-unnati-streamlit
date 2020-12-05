4315ff38684e7513841794317a944dbeefcaf9db
Prateek Senapati <prateek1210s@gmail.com>
2020-12-05 14:34:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.markdown('<style>body{text-align: center;}</style>', unsafe_allow_html=True)
 st.title('Krishi Unnati')
 st.header('A plant and crop disease detection app')
 st.text('')
-img = st.file_uploader(label='Upload image (PNG, JPG or JPEG)', type=['png', 'jpg', 'jpeg'])
+img = st.file_uploader(label='Upload leaf image (PNG, JPG or JPEG)', type=['png', 'jpg', 'jpeg'])
 if img is not None:
     st.image(image=img.read(), caption='Uploaded image')
     predict_button = st.button(label='Predict')
