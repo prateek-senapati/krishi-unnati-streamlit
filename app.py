4c218a87f742c6fcaa6baf6ea593e60e878598cb
Prateek Senapati <50922445+prateek-senapati@users.noreply.github.com>
2020-12-19 11:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,24 @@ st.markdown('<style>body{text-align: center;}</style>', unsafe_allow_html=True)
 
 # About section (sidebar)
 st.sidebar.subheader('About')
-st.sidebar.write('Krishi Unnati is a web application which can help you classify \
-plant and crop diseases using machine learning.')
+st.sidebar.write('Krishi Unnati is a web application which can help you classify plant and crop diseases using machine learning.')
 
 # Instructions section (sidebar)
 st.sidebar.subheader('Instructions to use')
-st.sidebar.write("Using the app is very simple. All you have to do is upload an \
-image of the diseased plant's (or crop's) leaf and click on the Predict button. \
-The app will use machine learning to predict the disease and display the result \
-along with a probability percentage.")
+st.sidebar.write("Using the app is very simple. All you have to do is upload an image of the diseased plant's (or crop's) leaf and click on the Predict button. \
+The app will use machine learning to predict the disease and display the result along with a probability percentage.")
 
 # Information section (sidebar)
 st.sidebar.subheader('More information')
 st.sidebar.write('As of now, the app can detect the following 39 classes:')
-st.sidebar.info('Apple - Apple scab, Apple - Black rot, Apple - Cedar apple rust, \
-Apple - healthy, Background without leaves, Blueberry - healthy,  Cherry - Powdery mildew, \
-Cherry - healthy, Corn - Cercospora leaf spot Gray leaf spot, Corn - Common rust, \
-Corn - Northern Leaf Blight, Corn - healthy, Grape - Black rot, Grape - Esca (Black Measles), \
-Grape - Leaf blight (Isariopsis Leaf Spot), Grape - healthy, Orange - Haunglongbing (Citrus greening), \
-Peach - Bacterial spot, Peach - healthy, Pepper, bell - Bacterial spot, Pepper, bell - healthy, \
-Potato - Early blight, Potato - Late blight, Potato - healthy, Raspberry - healthy, \
-Soybean - healthy, Squash - Powdery mildew, Strawberry - Leaf scorch, Strawberry - healthy, \
-Tomato - Bacterial spot, Tomato - Early blight, Tomato - Late blight, Tomato - Leaf Mold, \
-Tomato - Septoria leaf spot, Tomato - Spider mites Two-spotted spider mite, Tomato - Target Spot, \
-Tomato - Tomato Yellow Leaf Curl Virus, Tomato - Tomato mosaic virus, Tomato - healthy')
+st.sidebar.info('Apple - Apple scab, Apple - Black rot, Apple - Cedar apple rust, Apple - healthy, Background without leaves, Blueberry - healthy,  Cherry - Powdery mildew, \
+Cherry - healthy, Corn - Cercospora leaf spot Gray leaf spot, Corn - Common rust, Corn - Northern Leaf Blight, Corn - healthy, Grape - Black rot, Grape - Esca (Black Measles), \
+Grape - Leaf blight (Isariopsis Leaf Spot), Grape - healthy, Orange - Haunglongbing (Citrus greening), Peach - Bacterial spot, Peach - healthy, Pepper, bell - Bacterial spot, \
+Pepper, bell - healthy, Potato - Early blight, Potato - Late blight, Potato - healthy, Raspberry - healthy, Soybean - healthy, Squash - Powdery mildew, \
+Strawberry - Leaf scorch, Strawberry - healthy, Tomato - Bacterial spot, Tomato - Early blight, Tomato - Late blight, Tomato - Leaf Mold, Tomato - Septoria leaf spot, \
+Tomato - Spider mites Two-spotted spider mite, Tomato - Target Spot, Tomato - Tomato Yellow Leaf Curl Virus, Tomato - Tomato mosaic virus, Tomato - healthy')
 st.sidebar.write('We will be adding more disease classes to the app soon.')
+st.sidebar.write('We have also planned to add Indian languages to the user interface, as well as possible remedies of these diseases to the app.')
 st.sidebar.write('You can find the source code the app [here](https://github.com/prateek-senapati/krishi-unnati-streamlit).')
 
 # Main app interface
